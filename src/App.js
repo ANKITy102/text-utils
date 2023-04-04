62cc9dbf1cd2dfecc7ad3011ae1a8266ae1ee8d5
Ankit <mahesh9426346889@gmail.com>
2023-04-04 18:37:43 +0530

    commiting

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
-
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import React from 'react'
+// let name = "ankit"
 function App() {
+  const [mode, setDarkMode] = useState('light'); //whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
+  function showAlert(type, msg) {
+    setAlert({
+      type: type,
+      message: msg
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1800);
+  }
+  const toggleMode = () => {
+    if (mode === 'dark') {
+      setDarkMode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert("success", "Light mode has enabled")
+
+    }
+    else {
+      setDarkMode('dark');
+      document.body.style.backgroundColor = 'grey'
+      showAlert("success", "Dark mode has enabled")
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="Ankit" about="About us" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container">
+        <TextForm showAlert={showAlert} heading="Enter the text to analysis" mode={mode} />
+        <About />
+      </div>
+    </>
+
   );
 }
 
